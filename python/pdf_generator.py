9094ad27dc79b81243af573611938b2dc4ecf31a
Vaibhav Kesarkar <jarvice.robert@gmail.com>
2024-05-23 21:20:02 +0530

    added pdf genrator python code

---
+++ python/pdf_generator.py
@@ -0,0 +1,31 @@
+from fpdf import FPDF
+
+class PDF(FPDF):
+    def header(self):
+        self.set_font('Arial', 'B', 12)
+        self.cell(0, 10, 'Redis Cheat Sheet', 0, 1, 'C')
+
+    def chapter_title(self, title):
+        self.set_font('Arial', 'B', 12)
+        self.cell(0, 10, title, 0, 1, 'L')
+        self.ln(4)
+
+    def chapter_body(self, body):
+        self.set_font('Arial', '', 12)
+        self.multi_cell(0, 10, body)
+        self.ln()
+
+pdf = PDF()
+pdf.add_page()
+
+# Redis Cheat Sheet content
+content = """
+Your markdown
+"""
+
+pdf.chapter_body(content)
+pdf_output_path = "file_name.pdf"
+pdf.output(pdf_output_path)
+
+pdf_output_path
+
